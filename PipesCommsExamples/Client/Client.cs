4f762e483d879c61792f99ad86c920aa838ebef8
demastri <gear@demastri.com>
2015-02-27 18:50:11 -0600

    include apps to test anon pipes

---
+++ PipesCommsExamples/Client/Client.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.IO.Pipes;
+
+namespace Client
+{
+    class Client
+    {
+        static PipeStream pipeClientOut;
+        static StreamReader sr;
+        static StreamWriter StreamOut;
+        
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                using (PipeStream pipeClient =
+                    new AnonymousPipeClientStream(PipeDirection.In, args[0]))
+                {
+                    using (pipeClientOut =
+                        new AnonymousPipeClientStream(PipeDirection.Out, args[1]))
+                    {
+                        using (sr = new StreamReader(pipeClient))
+                        {
+                            StreamOut = new StreamWriter(pipeClientOut);
+                            // Read user input and send that to the client process. 
+                            StreamOut.AutoFlush = true;
+
+                            // Show that anonymous Pipes do not support Message mode. 
+                            try
+                            {
+                                ClientMessage("[CLIENT] Setting ReadMode to \"Message\".");
+                                pipeClient.ReadMode = PipeTransmissionMode.Message;
+                            }
+                            catch (NotSupportedException e)
+                            {
+                                ClientMessage("[CLIENT] Execption:\n    " + e.Message);
+                            }
+
+                            ClientMessage("[CLIENT] Current TransmissionMode: " + pipeClient.TransmissionMode.ToString() + ".");
+
+                            // Display the read text to the console 
+                            string temp;
+
+                            // Wait for 'sync message' from the server. 
+                            do
+                            {
+                                ClientMessage("[CLIENT] Wait for sync...");
+                                temp = sr.ReadLine();
+                            }
+                            while (!temp.StartsWith("SYNC"));
+
+                            ClientMessage("[CLIENT] Received sync...");
+
+                            // Read the server data and echo to the console. 
+                            do
+                            {
+                                //ClientMessage("[CLIENT] waiting for something...");
+                                temp = sr.ReadLine();
+                                //ClientMessage("[CLIENT] Got something...");
+                                if (temp != null)
+                                    ClientMessage("[CLIENT] Echo: " + temp);
+                            }
+                            while (!temp.StartsWith("quit"));
+                            ClientMessage("[CLIENT] quitting client process...");
+                        }
+                    }
+                }
+            }
+        }
+        static void ClientMessage(string msg)
+        {
+            bool usePipe = true;
+            if (!usePipe)
+                Console.WriteLine("[CLIENTCONSOLE] " +msg);
+            if (usePipe)
+            {
+                StreamOut.WriteLine(msg);
+                //pipeClientOut.WaitForPipeDrain();
+            }
+        }
+    }
+}
---
+++ PipesCommsExamples/Server/Server.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace Server
+{
+    class Server
+    {
+        static Process pipeClient;
+        static AnonymousPipeServerStream pipeServerOut;
+        static StreamWriter StreamOut;
+
+        static AnonymousPipeServerStream pipeServerIn;
+        static StreamReader StreamIn;
+
+        static string localBuffer;
+        static string processBuffer;
+
+        static void Main(string[] args)
+        {
+            InitProcess("C:\\Projects\\JPD\\BBRepos\\Chess\\PipesCommsExamples\\Client\\bin\\Debug\\Client.exe");
+            OpenPipes();
+            ConnectPipeToProcess();
+            CreateStreamOnPipes();
+
+            try
+            {
+                // Send a 'sync message' and wait for client to receive it.
+                WriteToStream("SYNC");
+
+                bool running = true;
+                while (running)
+                {
+                    running = CheckLocalIO();
+                    CheckProcessIO();
+                    running = HandleAllIO() && running;
+                }
+            }
+            // Catch the IOException that is raised if the pipe is broken or disconnected. 
+            catch (IOException e)
+            {
+                Console.WriteLine("[SERVER] Error: {0}", e.Message);
+            }
+
+            CleanupStreams();
+            CleanupPipes();
+            CleanupProcess();
+        }
+
+        static public void InitProcess(string procName)
+        {
+            pipeClient = new Process();
+            pipeClient.StartInfo.FileName = procName;
+
+        }
+
+        static public void OpenPipes()
+        {
+            pipeServerOut =
+               new AnonymousPipeServerStream(PipeDirection.Out,
+               HandleInheritability.Inheritable);
+
+            // Show that anonymous pipes do not support Message mode. 
+            try
+            {
+                Console.WriteLine("[SERVER] Setting ReadMode to \"Message\".");
+                pipeServerOut.ReadMode = PipeTransmissionMode.Message;
+            }
+            catch (NotSupportedException e)
+            {
+                Console.WriteLine("[SERVER] Exception:\n    {0}", e.Message);
+            }
+
+            Console.WriteLine("[SERVER] Current TransmissionMode: {0}.",
+                pipeServerOut.TransmissionMode);
+
+            pipeServerIn =
+               new AnonymousPipeServerStream(PipeDirection.In,
+               HandleInheritability.Inheritable);
+      
+        }
+        static public void ConnectPipeToProcess()
+        {
+            // Pass the client process a handle to the server.
+            pipeClient.StartInfo.Arguments =
+                pipeServerOut.GetClientHandleAsString() + " " + pipeServerIn.GetClientHandleAsString();
+
+            pipeClient.StartInfo.UseShellExecute = false;
+            pipeClient.Start();
+
+            pipeServerOut.DisposeLocalCopyOfClientHandle();
+            pipeServerIn.DisposeLocalCopyOfClientHandle();
+        }
+        static public void CreateStreamOnPipes()
+        {
+            StreamOut = new StreamWriter(pipeServerOut);
+            // Read user input and send that to the client process. 
+            StreamOut.AutoFlush = true;
+
+            StreamIn = new StreamReader(pipeServerIn);
+        }
+
+        static public void WriteToStream(string s)
+        {
+            //Console.WriteLine("[SERVER] Writing: <"+s+">.");
+            StreamOut.WriteLine(s);
+            //pipeServerOut.WaitForPipeDrain();
+        }
+
+        static Task<string> readAsync = null;
+        static async public void CheckProcessIO()
+        {
+            if( readAsync == null )
+                readAsync = StreamIn.ReadLineAsync();
+
+            if( readAsync.IsCompleted ) 
+            {
+                processBuffer = await readAsync;
+                readAsync.Dispose();
+                readAsync = null;
+            }
+            else
+                processBuffer = null;
+        }
+        static public bool CheckLocalIO()
+        {
+            if (Console.KeyAvailable)
+            {
+                localBuffer = Console.ReadLine();
+            }
+            return true;
+        }
+        static public bool HandleAllIO()
+        {
+            if (localBuffer != null)
+            {
+                // Send the console input to the client process.
+                WriteToStream(localBuffer);
+
+                if (localBuffer == "quit")
+                    return false;
+                Console.Write("[SERVER] Enter text: ");
+                localBuffer = null;
+            }
+            if (processBuffer != null)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("   [CLIENT] from process: " + processBuffer);
+                Console.Write("[SERVER] Enter text: ");
+                processBuffer = null;
+            }
+            return true;
+        }
+        static public void CleanupStreams()
+        {
+            if (StreamOut != null)
+                StreamOut.Dispose();
+            if (StreamIn != null)
+                StreamIn.Dispose();
+        }
+        static public void CleanupPipes()
+        {
+            if (pipeServerOut != null)
+                pipeServerOut.Dispose();
+        }
+        static public void CleanupProcess()
+        {
+            if (pipeClient != null)
+            {
+                pipeClient.WaitForExit();
+                pipeClient.Close();
+                Console.WriteLine("[SERVER] Client quit. Server terminating.");
+            }
+        }
+    }
+}
