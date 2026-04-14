7d5a6ce30466bb90afe06dbe809f7d42dbf367af
John DeMastri <gear@demastri.com>
2026-04-14 15:05:52 -0500

    made this more pythonic, lol

--- Utilities-Python/buildClubDir.py
+++ Utilities-Python/buildClubDir.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import time
 from datetime import datetime, timedelta
@@ -5,39 +6,40 @@ from threading import Thread
 
 from USCF.uscfRatingsApi import uscfRatingsApi
 
-
+global_counter = 1
 def main():
+    global global_counter
 
     eventJson = uscfRatingsApi.getEventsForAffiliate( sys.argv[1] if len(sys.argv) > 1 else None )
     endDate = datetime.now() - timedelta(days=180)
-    eventPlayers = set()
-    for event in eventJson['items']:
-        lastSeenDate = datetime.strptime(event['startDate'], "%Y-%m-%d")
-        if lastSeenDate < endDate:
-            break
-        print(event['startDate']+" = "+event['name'])
-        thisEventPlayers = uscfRatingsApi.getPlayersForEventFromUscfId(event['id'])
-        for player in thisEventPlayers:
-            eventPlayers.add(player['memberId'])
+    recentEvents = [event for event in eventJson["items"] if datetime.strptime(event['startDate'], "%Y-%m-%d") >= endDate]
+    playerSets = [uscfRatingsApi.getPlayersForEventFromUscfId(event['id']) for event in recentEvents]
+    eventPlayers = {player["memberId"] for playerList in playerSets for player in playerList}
 
     outFile = "./tsv/ClubDirOut.tsv"
     fOut = open(outFile, "w+")
 
-    counter = 1
-    for player in eventPlayers:
-        thisMember = uscfRatingsApi.getPlayerJson(player)
-        time.sleep(0.900)
-        outString = (str(counter)+"\t"+thisMember["id"]+"\t"+thisMember["lastName"].title()+", "+thisMember["firstName"].title()+"\t"+
-           next((str(obj["rating"]) for obj in thisMember["ratings"] if obj["ratingSystem"] == "R" and "rating" in obj), "NA")+"\t"+
-           next((str(obj["rating"]) for obj in thisMember["ratings"] if obj["ratingSystem"] == "Q" and "rating" in obj), "NA") + "\t" +
-           next((str(obj["rating"]) for obj in thisMember["ratings"] if obj["ratingSystem"] == "B" and "rating" in obj), "NA") + "\t" +
-           thisMember["expirationDate"])
-        fOut.write(outString+"\n")
-        print(outString)
-        counter = counter+1
+    global_counter = 1
+    [writePlayer(fOut, plr) for plr in eventPlayers]
     fOut.close()
 
     return 0
+
+def writePlayer(fOut, plr):
+    global global_counter
+
+    thisMember = uscfRatingsApi.getPlayerJson(plr)
+    time.sleep(0.900)
+    outString = (str(global_counter) + "\t" + thisMember["id"] + "\t" + thisMember["lastName"].title() + ", " + thisMember[
+        "firstName"].title() + "\t" +
+         next((str(obj["rating"]) for obj in thisMember["ratings"] if obj["ratingSystem"] == "R" and "rating" in obj), "NA") + "\t" +
+         next((str(obj["rating"]) for obj in thisMember["ratings"] if obj["ratingSystem"] == "Q" and "rating" in obj), "NA") + "\t" +
+         next((str(obj["rating"]) for obj in thisMember["ratings"] if obj["ratingSystem"] == "B" and "rating" in obj), "NA") + "\t" +
+         thisMember["expirationDate"])
+    fOut.write(outString + "\n")
+    print(outString)
+    global_counter = global_counter +1
+
 
 if __name__ == "__main__":
     main()
